5be3411e43134258f7f9a389d4e2292065ac1cda
pannzzz <mariazzz2326@gmail.com>
2025-03-09 21:35:58 -0500

    z

--- planos/floor-plan-app/backend/floorplan/admin.py
+++
@@ -1,5 +0,0 @@
-# floorplan/admin.py
-from django.contrib import admin
-from .models import Posicion
-
-admin.site.register(Posicion)
--- planos/floor-plan-app/backend/floorplan/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FloorplanConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'floorplan'
